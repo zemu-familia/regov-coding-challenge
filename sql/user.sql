55483a309e507fd0ea4ed49f6a6b824e778cd58d
zemu-familia <haziqaiman2@gmail.com>
2023-07-08 00:14:43 +0800

    database update

--- sql/user.sql
+++ sql/user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 07, 2023 at 06:25 AM
+-- Generation Time: Jul 07, 2023 at 04:13 PM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -31,10 +31,19 @@ CREATE TABLE `user` (
   `uniqid` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(256) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `about_me` varchar(4096) NOT NULL,
-  `about_me_draft` varchar(4096) NOT NULL
+  `about_me` varchar(4096) DEFAULT NULL,
+  `about_me_draft` varchar(4096) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`uniqid`, `username`, `password`, `about_me`, `about_me_draft`) VALUES
+(1, 'user', 'user', 'I am User', 'I am User draft'),
+(2, 'qwe', 'qwe', 'I am qwe.', NULL),
+(13, 'qwer', 'qwer', NULL, 'QWER Draft'),
+(14, 'qwert', 'qwert', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -44,7 +53,8 @@ CREATE TABLE `user` (
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`uniqid`);
+  ADD PRIMARY KEY (`uniqid`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -54,7 +64,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uniqid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `uniqid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
